0d98c3c4947984a57a2184a5314ca7cb09e75638
yamd1 <w5000c2d26@gmail.com>
2023-09-06 17:46:20 +0900

    add

--- .bin/shell_setup.sh
+++ .bin/shell_setup.sh
@@ -43,6 +43,10 @@ fi
 #----------------------------#
 if [ ! -d "~/../linuxbrew" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    sudo apt-get install build-essential
+    brew install gcc
 fi
 
 #----------------------------#
