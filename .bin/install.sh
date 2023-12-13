5bfe7715c97b575bf6b08960dabf7fe7446cfee7
yamd1 <yamd1.xx@gmail.com>
2023-12-13 15:03:29 +0900

    :bug: fix install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ue
 
-ln -snf $HOME/dotfiles/.local $HOME
+cp -R $HOME/dotfiles/.local $HOME
 ln -snf $HOME/dotfiles/.config $HOME
 ln -snf $HOME/dotfiles/.zshrc $HOME
 ln -snf $HOME/dotfiles/.zshenv $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
 
-./.bin/install-aqua.sh
+$HOME/dotfiles/.bin/install-aqua.sh
