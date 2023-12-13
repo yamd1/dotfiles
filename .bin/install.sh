1191819030daa627be65faa78d8c86f2e6cb6add
yamd1 <w5000c2d26@gmail.com>
2023-12-13 21:15:26 +0900

    :bug: ログインしてすぐに実行ファイルが存在しないエラーが出ないように

--- .bin/install.sh
+++ .bin/install.sh
@@ -8,4 +8,16 @@ ln -snf $HOME/dotfiles/.zshenv $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
 
-$HOME/dotfiles/.bin/install-aqua.sh
+# $HOME/dotfiles/.bin/install-aqua.sh
+
+# SEE: https://aquaproj.github.io/docs/products/aqua-installer#shell-script
+if [[ ! -x "$HOME/.local/share/aquaproj-aqua/bin/aqua" ]]; then
+  curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.2.0/aqua-installer
+  echo "d13118c3172d90ffa6be205344b93e8621de9bf47c852d80da188ffa6985c276  aqua-installer" | sha256sum -c
+  chmod +x aqua-installer
+  ./aqua-installer
+fi
+
+AQUA_CONFIG=$HOME/.config/aquaproj-aqua
+export AQUA_POLICY_CONFIG=$AQUA_CONFIG/policy.yaml
+exec $HOME/.local/share/aquaproj-aqua/bin/aqua -c $AQUA_CONFIG/aqua.yaml i
