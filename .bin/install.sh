5ac24e3bf0da3cfa5b66092020594476a0ffbe43
yamd1 <yamd1.xx@gmail.com>
2023-12-13 15:10:16 +0900

    :bug: override .config

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,7 +2,7 @@
 set -ue
 
 cp -R $HOME/dotfiles/.local $HOME
-ln -snf $HOME/dotfiles/.config $HOME
+cp -R $HOME/dotfiles/.config $HOME
 ln -snf $HOME/dotfiles/.zshrc $HOME
 ln -snf $HOME/dotfiles/.zshenv $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
