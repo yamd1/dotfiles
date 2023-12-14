55ed5763b792ce574eac0a334b509eb7f35f9c80
yamd1 <w5000c2d26@gmail.com>
2023-12-14 21:34:24 +0900

    :bug: fix install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ue
 
+[[ -d "$HOME/.local" ]] && rm -rf "$HOME/.local"
 cp -fR $HOME/dotfiles/.local $HOME
+
+[[ -d "$HOME/.config" ]] && rm -rf "$HOME/.config"
 cp -fR $HOME/dotfiles/.config $HOME
+
 ln -snf $HOME/dotfiles/.zshrc $HOME
 ln -snf $HOME/dotfiles/.zshenv $HOME
 ln -snf $HOME/dotfiles/.gitconfig $HOME
