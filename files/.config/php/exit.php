d48ed6680869274ea0aa0aa6c9aebdb4f82bd4a2
yamd1 <w5000c2d26@gmail.com>
2024-01-25 21:31:29 +0900

    🚚 phpファイルのパスを変更

--- files/.config/php/exit.php
+++ files/.config/php/exit.php
@@ -3,6 +3,6 @@
 /**
  * VSCode.settings: code-runner.executorMap
  * Laravel開発時に利用する
- * code-runnerを用いてバッファのPHPコードをtinkerで実行し、このファイルが実行されてtinkerを終了する
+ * code-runnerを用いてバッファのPHPコードをtinkerで実行しこのファイルが実行されてtinkerを終了する
  */
 exit;
