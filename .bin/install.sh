502c850b31465d9c54fa2bdac46c4179b22e19d1
yamd1 <w5000c2d26@gmail.com>
2024-03-01 10:11:49 +0900

    🔨 (wsl): weztermを任意のUserディレクトリ配下にコピーできるように

--- .bin/install.sh
+++ .bin/install.sh
@@ -17,8 +17,7 @@ ln -snf $DOT_DIR/files/.config/.lnav $HOME
 $DOT_DIR/.bin/install-aqua.sh
 
 if [[ "${1:-}" == "wsl" ]]; then
-  # Symbolic links don't work between WSL2 and Windows, so use cp instead
-  \ls /mnt/c/Users | fzf | xargs -I_ cp $DOT_DIR/files/.config/wezterm/.wezterm.lua /mnt/c/Users/_/.wezterm.lua
+  $DOT_DIR/.bin/setup-windows.sh
 fi
 
 $(which zsh)
