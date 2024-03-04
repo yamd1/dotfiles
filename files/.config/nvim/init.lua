89006e26646e12691a3edd1a5dff43bfe968aed1
yamd1 <w5000c2d26@gmail.com>
2024-03-04 18:44:15 +0900

    :wrench: nvim設定を分離

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -8,30 +8,5 @@ require("lspsetting")
 require("git-settings")
 require("clipboard")
 require("_nvim-tree")
+require("options")
 
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.netrw_dirhistmax = 0
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.textwidth = 0
-vim.opt.autoindent = true
-vim.opt.hlsearch = true
-vim.opt.swapfile = false
-vim.opt.wrap = false
-vim.scriptencoding = 'utf-8'
-vim.opt.background = "dark"
-vim.opt.re = 0
-vim.opt.hidden = true
-vim.g.editorconfig = true
-vim.opt.list = true
-vim.listchars = {
-    tab = '>>',
-    trail = '-',
-    nbsp = '+'
-}
-vim.opt.termguicolors = true
-vim.opt.winblend = 0 -- ウィンドウの不透明度
-vim.opt.pumblend = 0 -- ポップアップメニューの不透明度
