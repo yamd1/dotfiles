85101172bde92d94ac0ced9025b7d95367409473
yamd1 <w5000c2d26@gmail.com>
2024-03-06 11:32:24 +0900

    :technologist: コンテナ内のプロンプトを変更するように

--- .bin/install.sh
+++ .bin/install.sh
@@ -9,6 +9,10 @@ ln -snf $DOT_DIR/files/.local $HOME
 [[ -d "$HOME/.config" ]] && rm -rf "$HOME/.config"
 ln -snf $DOT_DIR/files/.config $HOME
 
+[[ "$USER" == "appuser" ]] && \
+    cp $DOT_DIR/files/.config/starship/remote_starship.toml $XDG_CONFIG_HOME/starship.toml || \
+    cp $DOT_DIR/files/.config/starship/host_starship.toml $XDG_CONFIG_HOME/starship.toml
+
 ln -snf $DOT_DIR/files/.config/zsh/.zshrc $HOME
 ln -snf $DOT_DIR/files/.config/zsh/.zshenv $HOME
 ln -snf $DOT_DIR/files/.config/git/.gitconfig $HOME
