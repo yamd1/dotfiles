e519c060aaa4abe8d8f2df59e31ce36e7c049d8d
yamd1 <w5000c2d26@gmail.com>
2024-03-11 16:07:42 +0900

    :technologist: (nvim): 新規バッファを開くキーマップ

--- files/.config/nvim/lua/keymap.lua
+++ files/.config/nvim/lua/keymap.lua
@@ -9,6 +9,7 @@ vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('n', '<C-q>', ':q<CR>')
 vim.keymap.set('n', '<C-Q>', ':q!<CR>')
 vim.keymap.set('n', '<C-w>', ':bd<CR>:bfirst<CR>')
+vim.keymap.set('n', '<Leader>ee', ':enew<CR>')
 
 -- Double press the j key to Esc key
 vim.keymap.set('i', 'jk', '<Esc>', {
