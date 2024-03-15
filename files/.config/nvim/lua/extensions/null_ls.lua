3428fe6ea6b920041127f49cd7692a0051d71b38
yamd1 <w5000c2d26@gmail.com>
2024-03-15 10:00:53 +0900

    :heavy_plus_sign: (nvim): nvim-lsp

--- files/.config/nvim/lua/extensions/null_ls.lua
+++ files/.config/nvim/lua/extensions/null_ls.lua
@@ -1,17 +1,35 @@
--- local null_ls = require("null-ls")
--- null_ls.setup({
---     sources = {
---         null_ls.builtins.diagnostics.phpstan.with({
---             command = "/opt/app/vendor/bin/phpstan",
---             args = { "analyze", "--error-format", "json", "--no-progress", "--memory-limit=-1", "$FILENAME" }
---         }),
---         null_ls.builtins.formatting.pint.with({
---             -- prefer_local = "vendor/bin"
---             command = "/opt/app/vendor/bin/pint"
---         }),
---         null_ls.builtins.formatting.prettier.with({
---             filetypes = { "js", "ts", "json", "yaml", "yml", "md" },
---             prefer_local = "node_modules/.bin"
---         }),
---     },
--- })
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.phpstan.with({
+            command = "/opt/app/vendor/bin/phpstan",
+            args = { "analyze", "--error-format", "json", "--no-progress", "--memory-limit=-1", "$FILENAME" },
+        }),
+        null_ls.builtins.formatting.pint.with({
+            -- prefer_local = "vendor/bin"
+            command = "/opt/app/vendor/bin/pint",
+        }),
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "js", "ts", "json", "yaml", "yml", "md" },
+            command = "/opt/app/node_modules/.bin",
+        }),
+    },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({
+                        bufnr = bufnr,
+                        filter = function(client)
+                            return client.name == "null-ls"
+                        end,
+                    })
+                end,
+            })
+        end
+    end,
+})
