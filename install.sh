b0ab9e1ac6b1fd06f15f9a7637e73bf39c3fc6d0
yamd1 <w5000c2d26@gmail.com>
2024-03-16 14:30:11 +0900

    :wrench: (install): OSがdebian系のときはapt-getを用いてwl-clipboardをインストールするように

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ ln -snf $DOT_DIR/files/.local $HOME
 [[ -d "$HOME/.config" ]] && rm -rf "$HOME/.config"
 ln -snf $DOT_DIR/files/.config $HOME
 
-if ! "${IS_CONTAINER}"; then
+if "${IS_CONTAINER:-false}"; then
+  cp $DOT_DIR/files/.config/starship/remote_starship.toml $HOME/.config/starship.toml
+else
   cp $DOT_DIR/files/.config/starship/host_starship.toml $HOME/.config/starship.toml
 fi
 
@@ -22,6 +24,24 @@ $DOT_DIR/.bin/install-aqua.sh
 
 if [[ "${1:-}" == "wsl" ]]; then
   $DOT_DIR/.bin/setup-windows.sh
+fi
+
+if [[ -n "$WAYLAND_DISPLAY" ]]; then
+  while read os; do
+    case "$os" in
+      debian)
+        sudo apt-get update
+        sudo apt-get install -y wl-clipboard
+        ;;
+      lsb) # Ubuntu
+        sudo apt-get update
+        sudo apt-get install -y wl-clipboard
+        ;;
+      *)
+        :
+        ;;
+    esac
+  done < <(ls /etc/*release | grep -o '\/.*[_-]release' | sed "s/\/etc\/\(.*\)[_-]release/\1/g")
 fi
 
 $(which zsh)
