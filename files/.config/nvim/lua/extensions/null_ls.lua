de4b1461dabf01ea1973a7363c24c19848b4063b
yamd1 <w5000c2d26@gmail.com>
2024-03-17 10:58:58 +0900

    :adhesive_bandage: (nvim): formatter

--- files/.config/nvim/lua/extensions/null_ls.lua
+++ files/.config/nvim/lua/extensions/null_ls.lua
@@ -25,12 +25,8 @@ null_ls.setup({
                         bufnr = bufnr,
                         -- SEE: https://minerva.mamansoft.net/Notes/%F0%9F%93%9DNeovim%E3%81%A7nvim-lspconfig%E3%81%A8none-ls%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E4%BF%9D%E5%AD%98%E6%99%82%E3%81%AB%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88%E3%81%99%E3%82%8B%E3%81%A8%E7%95%B0%E3%81%AA%E3%82%8B%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88%E3%81%8C2%E5%BA%A6%E3%81%8B%E3%81%8B%E3%82%8B%E3%81%93%E3%81%A8%E3%81%8C%E3%81%82%E3%82%8B
                         filter = function(client)
-
                             local disabled_format_clients = { "intelephense" }
-                                -- return not vim.tbl_contains(disabled_format_clients, c.name)
-                            return client.name ~= "intelephense"
-
-
+                            return not vim.tbl_contains(disabled_format_clients, client.name)
                         end,
                     })
                 end,
