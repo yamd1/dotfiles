87b72a3292a691dcccbd44ac8580bbd2651ef8fb
yamd1 <w5000c2d26@gmail.com>
2024-03-22 11:41:30 +0900

    :recycle: SaveOnWithoutFormatをリファクタリング

--- files/.config/nvim/lua/extensions/null_ls.lua
+++ files/.config/nvim/lua/extensions/null_ls.lua
@@ -16,15 +16,11 @@ null_ls.setup({
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.g.format_on_save = true
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function(client)
-                    if vim.g.format_on_save == false then
-                        return
-                    end
                     vim.lsp.buf.format({
                         bufnr = bufnr,
                         -- SEE: https://minerva.mamansoft.net/Notes/%F0%9F%93%9DNeovim%E3%81%A7nvim-lspconfig%E3%81%A8none-ls%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E4%BF%9D%E5%AD%98%E6%99%82%E3%81%AB%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88%E3%81%99%E3%82%8B%E3%81%A8%E7%95%B0%E3%81%AA%E3%82%8B%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88%E3%81%8C2%E5%BA%A6%E3%81%8B%E3%81%8B%E3%82%8B%E3%81%93%E3%81%A8%E3%81%8C%E3%81%82%E3%82%8B
