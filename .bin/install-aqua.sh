e4f23c83f4ac68b34700d827894a09a9f707fc44
yamd1 <w5000c2d26@gmail.com>
2024-03-23 21:06:36 +0900

    :arrow_up: aqua

--- .bin/install-aqua.sh
+++ .bin/install-aqua.sh
@@ -3,12 +3,13 @@ set -ue
 
 # SEE: https://aquaproj.github.io/docs/products/aqua-installer#shell-script
 if [[ ! -x "$HOME/.local/share/aquaproj-aqua/bin/aqua" ]]; then
-  curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.2.0/aqua-installer
-  echo "d13118c3172d90ffa6be205344b93e8621de9bf47c852d80da188ffa6985c276  aqua-installer" | sha256sum -c
+  curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.3.0/aqua-installer
+  echo "1577b99b74751a5ddeea757198cee3b600fce3ef18990540e4d0e667edcf1b5f  aqua-installer" | sha256sum -c
   chmod +x aqua-installer
   ./aqua-installer
 fi
 
 AQUA_CONFIG=$HOME/.config/aquaproj-aqua
 export AQUA_POLICY_CONFIG=$AQUA_CONFIG/policy.yaml
+# $HOME/.local/share/aquaproj-aqua/bin/aqua allow policy
 $HOME/.local/share/aquaproj-aqua/bin/aqua -c $AQUA_CONFIG/aqua.yaml i
