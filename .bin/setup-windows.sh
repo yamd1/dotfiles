3bdc027097f4f58ab1f36332aeecb807239f786f
yamd1 <w5000c2d26@gmail.com>
2024-03-23 09:20:04 +0900

    :sparkles: Dockerコンテナ内からブラウザを開けるように

--- .bin/setup-windows.sh
+++ .bin/setup-windows.sh
@@ -5,5 +5,7 @@ set -ue
 # Symbolic links don't work between WSL2 and Windows, so use cp instead
 \ls /mnt/c/Users | fzf | xargs -I_ cp $DOT_DIR/files/.config/wezterm/.wezterm.lua /mnt/c/Users/_/.wezterm.lua
 
-# Install wl-clipboard
-sudo apt-get install wl-clipboard
+sudo apt-get update
+
+sudo apt-get install -y wl-clipboard ubuntu-wsl wslu
+
