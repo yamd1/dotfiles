f7d4135073c58c07ffd76ee5c88b0d6ffcd3683d
yamd1 <w5000c2d26@gmail.com>
2024-04-03 20:52:14 +0900

    :technologist: (nvim): formatter

--- files/.config/nvim/lua/plugins.lua
+++ files/.config/nvim/lua/plugins.lua
@@ -90,8 +90,8 @@ local plugins = {
     { "nvim-tree/nvim-web-devicons" },
     { "lukas-reineke/indent-blankline.nvim" },
     { "nvim-lualine/lualine.nvim" },
-    { "akinsho/bufferline.nvim" },
     { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+    { "cappyzawa/trim.nvim" },
     { "CRAG666/code_runner.nvim" },
     { "github/copilot.vim" },
     {
