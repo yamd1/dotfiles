0a3d4660d75e09e793f92487235096086d307c7c
yamd1 <w5000c2d26@gmail.com>
2024-04-07 12:42:32 +0900

    :coffin: (nvim): Removed unnecessary comments.

--- files/.config/nvim/lua/extensions/colorscheme.lua
+++ files/.config/nvim/lua/extensions/colorscheme.lua
@@ -1,27 +1,26 @@
-require('kanagawa').setup({
-    compile = false,             -- enable compiling the colorscheme
-    undercurl = true,            -- enable undercurls
+require("kanagawa").setup({
+    compile = false,
+    undercurl = true,
     commentStyle = { italic = true },
     functionStyle = {},
-    keywordStyle = { italic = true},
+    keywordStyle = { italic = true },
     statementStyle = { bold = true },
     typeStyle = {},
-    transparent = false,         -- do not set background color
-    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-    colors = {                   -- add/modify theme and palette colors
+    transparent = false,   -- do not set background color
+    dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true, -- define vim.g.terminal_color_{0,17}
+    colors = {             -- add/modify theme and palette colors
         palette = {},
         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
     },
     overrides = function(colors) -- add/modify highlights
         return {}
     end,
-    theme = "dragon",              -- Load "wave" theme when 'background' option is not set
-    background = {               -- map the value of 'background' option to a theme
-        dark = "dragon",           -- try "dragon" !
-        light = "lotus"
+    theme = "dragon", -- Load "wave" theme when 'background' option is not set
+    background = {    -- map the value of 'background' option to a theme
+        dark = "dragon",
+        light = "lotus",
     },
 })
 
--- setup must be called before loading
 vim.cmd("colorscheme kanagawa")
