b9a7b0e4307c62466e4a8714b792d989e299ea1b
yamd1 <w5000c2d26@gmail.com>
2024-06-28 04:50:51 +0000

    :technologist: eslintのフォーマット(インポート順のソート)が有効になるように

--- files/.config/nvim/lua/autocmd.lua
+++ files/.config/nvim/lua/autocmd.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        -- Since conform does not format when using eslint, use lsp.buf.format instead
+        vim.lsp.buf.format({ async = true })
+
+        -- Format with conform.format() after LSP's formatting to achieve the desired format (e.g., intelephense -> pint)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
