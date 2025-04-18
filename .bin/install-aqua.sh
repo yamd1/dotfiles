c8cb8271e88dcfbe3668bea7ce00d4760d1c31c9
yamd1 <w5000c2d26@gmail.com>
2025-04-18 13:29:04 +0900

    👽 (aqua): v2.48.2から発生するSLSA検証失敗を回避するためv2.48.1に固定する

--- .bin/install-aqua.sh
+++ .bin/install-aqua.sh
@@ -6,7 +6,7 @@ if [[ ! -x "$HOME/.local/share/aquaproj-aqua/bin/aqua" ]]; then
   curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.1.1/aqua-installer
   echo "e9d4c99577c6b2ce0b62edf61f089e9b9891af1708e88c6592907d2de66e3714  aqua-installer" | sha256sum -c -
   chmod +x aqua-installer
-  ./aqua-installer
+  ./aqua-installer -v v2.48.1
 fi
 
 AQUA_CONFIG=$HOME/.config/aquaproj-aqua
