4378313608a858bcf1dca674fa59b3c2dc7dfc32
yamd1 <w5000c2d26@gmail.com>
2025-06-24 01:42:13 -0700

    :sparkles: add mac support

--- .bin/setup-windows.sh
+++ .bin/setup-windows.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -ue
 
+# This script is WSL-specific and should not run on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "This script is for WSL only. Skipping on macOS."
+    exit 0
+fi
+
 # Symbolic links don't work between WSL2 and Windows, so use cp instead
 \ls /mnt/c/Users | fzf | xargs -I_ cp $HOME/dotfiles/files/.config/wezterm/.wezterm.lua /mnt/c/Users/_/.wezterm.lua
